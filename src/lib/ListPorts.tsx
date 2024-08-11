56942dc9a122ae352c9b02ab3de730ee6e460447
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2024-08-11 16:58:54 +0200

    Remove unused components

--- src/lib/ListPorts.tsx
+++
@@ -1,9 +0,0 @@
-export async function listPorts(type) {
-  return new Promise((resolve, reject) => {
-    window.electronAPI.handlePortList((_, newPortList) => {
-      console.log("new ports list", newPortList);
-      resolve(newPortList);
-    });
-    window.electronAPI.listPorts(type);
-  });
-}
